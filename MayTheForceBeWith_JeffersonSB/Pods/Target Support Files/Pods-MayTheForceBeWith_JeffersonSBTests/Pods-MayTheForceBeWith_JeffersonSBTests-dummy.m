7a11dc59213d49ac0e4d2706b7f7cd7757369bad
Jefferson Batista <jeffsouzabatista@gmail.com>
2020-02-11 10:12:34 +0000

    it was updated the project structure

--- MayTheForceBeWith_JeffersonSB/Pods/Target Support Files/Pods-MayTheForceBeWith_JeffersonSBTests/Pods-MayTheForceBeWith_JeffersonSBTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_MayTheForceBeWith_JeffersonSBTests : NSObject
-@end
-@implementation PodsDummy_Pods_MayTheForceBeWith_JeffersonSBTests
-@end
